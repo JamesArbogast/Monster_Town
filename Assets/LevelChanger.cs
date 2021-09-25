31fb3fa071b7917676910b71a2da6ee87de08d22
James Arbogast <jamesarbogast52@gmail.com>
2021-09-24 23:59:08 -0700

    fades

--- Assets/LevelChanger.cs
+++ Assets/LevelChanger.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelChanger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public Animator anim;
+
+    private int lvlToLoad;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            FadeToLevel(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    public void FadeToLevel(int lvlIndex)
+    {
+        lvlToLoad = lvlIndex;
+        anim.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(lvlToLoad);
     }
 }
