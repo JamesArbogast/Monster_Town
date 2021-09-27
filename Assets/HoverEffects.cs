4f52cbca2b549ea33ad773f70a850534eeb899f0
James Arbogast <jamesarbogast52@gmail.com>
2021-09-26 20:53:54 -0700

    Updated red color change on menu

---
+++ Assets/HoverEffects.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HoverEffects : MonoBehaviour
+{
+    public void FullOpacity()
+    {
+        int opacity = ((int)this.GetComponent<Image>().color.a);
+        opacity = 255;
+    }
+
+
+}
