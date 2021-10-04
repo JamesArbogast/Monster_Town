aff5b538401e3cd0b34f3bac5513cf31a44fae21
James Arbogast <jamesarbogast52@gmail.com>
2021-10-03 22:45:57 -0700

    base movement finished

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
     public Vector2 movement;
+    public Animator anim;
 
     // Update is called once per frame
     void Update()
@@ -15,6 +16,20 @@ public class PlayerMovement : MonoBehaviour
         //Input
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        anim.SetFloat("Horizontal", movement.x);
+        anim.SetFloat("Vertical", movement.y);
+        anim.SetFloat("Speed", movement.sqrMagnitude);
+
+        //fix to left movement sprites
+        if (movement.x == -1)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
     }
 
     private void FixedUpdate()
