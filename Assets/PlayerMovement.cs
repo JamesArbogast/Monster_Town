dfac03dad4468d1c0394c3ff546ec0b800c9de79
James Arbogast <jamesarbogast52@gmail.com>
2021-10-03 23:19:02 -0700

    dodge roll added to movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -10,6 +10,18 @@ public class PlayerMovement : MonoBehaviour
     public Vector2 movement;
     public Animator anim;
 
+
+    //extra movements
+    private Vector3 slideDir;
+    private Vector3 newSpot;
+    private float slideSpeed;
+    private State state;
+    private enum State
+    {
+        Normal,
+        DodgeRollSliding,
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -30,11 +42,60 @@ public class PlayerMovement : MonoBehaviour
         {
             GetComponent<SpriteRenderer>().flipX = false;
         }
+
+        switch(state)
+        {
+            case State.Normal:
+                //dodge roll
+                HandleDodgeRoll();
+                break;
+            case State.DodgeRollSliding:
+                HandleDodgeRollSliding();
+                break;
+        }
+
+        //roll directions
+        if(movement.x == 1)
+        {
+            newSpot = new Vector2(transform.position.x + 2, transform.position.y);
+        }
+        else if (movement.x == -1)
+        {
+            newSpot = new Vector2(transform.position.x - 2, transform.position.y);
+        }
+        else if (movement.y == 1)
+        {
+            newSpot = new Vector2(transform.position.x, transform.position.y + 2);
+        }
+        else if (movement.y == -1)
+        {
+            newSpot = new Vector2(transform.position.x, transform.position.y - 2);
+        }
     }
 
     private void FixedUpdate()
     {
         //Movement
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void HandleDodgeRoll()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            state = State.DodgeRollSliding;
+            slideDir = (newSpot - transform.position).normalized;
+            slideSpeed = 100f;
+        }
+    }
+
+    private void HandleDodgeRollSliding()
+    {
+        transform.position += slideDir * slideSpeed * Time.deltaTime;
+        slideSpeed -= slideSpeed * 10f * Time.deltaTime;
+        if(slideSpeed < 5f)
+        {
+            state = State.Normal;
+        }
     }
 }
