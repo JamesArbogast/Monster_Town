919747aed59a1821a272b832418cf33caa5dafb3
James Arbogast <jamesarbogast52@gmail.com>
2021-10-05 22:43:22 -0700

    camera movement added

---
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public Transform target;
+    public float smoothing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // late update used to make sure camera movement doesnt fire before player movement
+    void LateUpdate()
+    {
+        if(transform.position != target.position)
+        {
+            Vector3 targetPos = new Vector3(target.position.x, target.position.y, transform.position.z);
+            //linear interpolation
+            transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);
+        }
+    }
+}
