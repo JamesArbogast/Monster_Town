20d55217c147438c8306e5e3665cba7430818353
James Arbogast <jamesarbogast52@gmail.com>
2021-10-21 15:59:33 -0700

    Base item started

---
+++ Assets/Scripts/Items/ItemMain/Item.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    public enum ItemTypes
+    {
+        EQUIPMENT,
+        POWERUP,
+        STORY,
+        MATERIAL,
+        PLACEABLE
+    }
+
+    public ItemTypes itemType;
+
+    public Item(ItemTypes itemType)
+    {
+        itemType = this.itemType;
+    }
+}
