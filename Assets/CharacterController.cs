a2f4a83944d2f9ea2ff5faca2928b9db9a4cf17e
James Arbogast <jamesarbogast52@gmail.com>
2024-10-21 23:50:27 -0700

    re-configured player movement through rigidbody, interpolation, re-added animator

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -4,23 +4,54 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    private const float MOVE_SPEED = 10f;
+    private Rigidbody2D rgdbdy2D;
+    private Vector2 moveSpeed;
+    private Vector3 moveDir;
+    private Animator anim;
+
+    private void Awake()
+    {
+        rgdbdy2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
     private void Update()
     {
+
+        float moveX = 0f;
+        float moveY = 0f;
+
         if(Input.GetKey(KeyCode.W))
         {
-            transform.position += new Vector3(0, +1);
+            moveY = +1f;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position += new Vector3(0, -1);
+            moveY = -1f;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += new Vector3(-1, 0);
+            moveX = -1f;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += new Vector3(+1, 0);
+            moveX = +1f;
         }
+
+        // normalized move direction
+        moveDir = new Vector3(moveX, moveY).normalized;
+
+        anim.SetFloat("Horizontal", moveX);
+        anim.SetFloat("Vertical", moveY);
+        //Input
+        moveSpeed.x = Input.GetAxisRaw("Horizontal");
+        moveSpeed.y = Input.GetAxisRaw("Vertical");
+        anim.SetFloat("Speed", moveSpeed.sqrMagnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        rgdbdy2D.velocity = moveDir * MOVE_SPEED;
     }
 }
