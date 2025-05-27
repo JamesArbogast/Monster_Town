a769cb3fc3fe1bb97aa10e9e698a0629f2540167
James Arbogast <jamesarbogast52@gmail.com>
2025-05-26 23:13:27 -0700

    added movement script to monster, bug causing monster to circle player

---
+++ Assets/Scripts/Monsters/MonsterMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    public float rotationSpeed;
+    private Rigidbody2D _rigidbody;
+    private PlayerAwarenessController playerAwarenessController;
+    private Vector2 targetDirection;
+
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        playerAwarenessController = GetComponent<PlayerAwarenessController>();
+
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        UpdateTargetDirection();
+        RotateTowardsTarget();
+        SetVelocity();
+    }
+
+    private void UpdateTargetDirection()
+    {
+        if(playerAwarenessController.AwareOfPlayer)
+        {
+            targetDirection = playerAwarenessController.DirectionToPlayer;
+        }
+        else
+        {
+            targetDirection = Vector2.zero;
+        }
+    }
+
+    private void RotateTowardsTarget()
+    {
+        if (targetDirection == Vector2.zero)
+        {
+            return;
+        }
+
+        Quaternion targetRotation = Quaternion.LookRotation(transform.forward, targetDirection);
+        Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        _rigidbody.SetRotation(rotation);
+    }
+
+    private void SetVelocity()
+    {
+        if (targetDirection == Vector2.zero)
+        {
+            _rigidbody.linearVelocity = Vector2.zero;
+        }
+        else
+        {
+            _rigidbody.linearVelocity = transform.up * speed;
+        }
+    }
+}
---
+++ Assets/Scripts/Monsters/PlayerAwarenessController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAwarenessController : MonoBehaviour
+{
+
+    public bool AwareOfPlayer { get; private set; }
+    public Vector2 DirectionToPlayer { get; private set; }
+    [SerializeField]
+    private float playerAwarenessDistance;
+    [SerializeField]
+    private Transform player;
+
+    private void Awake()
+    {
+        player = FindAnyObjectByType<CharacterController>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 enemyToPlayerVector = player.position - transform.position;
+        DirectionToPlayer = enemyToPlayerVector.normalized;
+        Debug.Log(enemyToPlayerVector);
+
+        Debug.Log(enemyToPlayerVector.magnitude);
+        if (DirectionToPlayer.magnitude <= playerAwarenessDistance)
+        {
+            AwareOfPlayer = true;
+            Debug.Log("Aware of player!");
+        }
+        else
+        {
+            AwareOfPlayer = false;
+            Debug.Log("Not aware of player!");
+        }
+    }
+}
