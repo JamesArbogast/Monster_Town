17e290302aee4d17960a4474c2c46d54163d1fc1
James Arbogast <jamesarbogast52@gmail.com>
2025-08-31 22:22:00 -0700

    init setup for field of view for player

--- Assets/Scripts/Player/PlayerFieldOfView.cs
+++ Assets/Scripts/Player/PlayerFieldOfView.cs
@@ -2,30 +2,49 @@ using UnityEngine;
 
 public class PlayerFieldOfView : MonoBehaviour
 {
+    void Awake()
+    {
+        // Change "Default" to the name of your desired sorting layer
+        GetComponent<MeshRenderer>().sortingLayerName = "FieldofView";
+        // Set the rendering order within that layer (a higher number is more "in front")
+        GetComponent<MeshRenderer>().sortingOrder = 0;
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
         Mesh mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
 
-        Vector3[] vertices = new Vector3[3];
-        Vector2[] uv = new Vector2[3];
-        int[] triangles = new int[3];
+        float fov = 90f;
+        Vector3 origin = Vector3.zero;
+        int rayCount = 2;
+        float angle = 0f;
+        float angleIncrease = fov / rayCount;
+        float viewDistance = 50f;
+
+        Vector3[] vertices = new Vector3[rayCount + 1 + 1];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[rayCount * 3];
+
+        vertices[0] = origin;
+
+        for(int i =0; i <=rayCount; i++)
+        {
+
+        }
 
         vertices[0] = Vector3.zero;
-        vertices[1] = new Vector3();
-        vertices[2] = Vector3.zero;
+        vertices[1] = new Vector3(50, 0);
+        vertices[2] = new Vector3(0, -50);
+
+        triangles[0] = 0;
+        triangles[1] = 1;
+        triangles[2] = 2;
 
         mesh.vertices = vertices;
         mesh.uv = uv;
         mesh.triangles = triangles;
-
-
-
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
